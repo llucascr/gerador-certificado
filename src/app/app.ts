6866baba341faf3e04615e84e78fe1401fc73f7c
llucascr <lucasdecamposranzani@gmail.com>
2025-09-17 18:33:27 -0300

    feat: desenvolvimento do html e css do coponente itemCertificado

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Navbar } from "./_components/navbar/navbar";
 import { PrimaryButton } from './_components/primary-button/primary-button';
 import { SecondaryButton } from './_components/secondary-button/secondary-button';
+import { ItemCertificado } from './_components/item-certificado/item-certificado';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton],
+  imports: [RouterOutlet, Navbar, PrimaryButton, SecondaryButton, ItemCertificado],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
